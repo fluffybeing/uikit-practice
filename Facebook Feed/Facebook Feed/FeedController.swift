d6b5f6aa08a156ea5adb59574943cde9c99429a5
Rahul Ranjan <rahul.rrixe@gmail.com>
2017-04-30 23:16:02 +0530

    create initial template

---
+++ Facebook Feed/Facebook Feed/FeedController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facebook Feed
+//
+//  Created by Rahul Ranjan on 4/30/17.
+//  Copyright © 2017 Rahul Ranjan. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.gray
+    }
+
+}
+
