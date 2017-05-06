b0c46b47dbfe69aa414d2abe4388cc26b144034b
Rahul Ranjan <rahul.rrixe@gmail.com>
2017-05-06 17:30:51 +0530

    complete image fetch

---
+++ GameOfChats/GameOfChats/Extensions+ImageCache.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions+ImageCache.swift
+//  GameOfChats
+//
+//  Created by Rahul Ranjan on 5/6/17.
+//  Copyright © 2017 Rahul Ranjan. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+}
--- GameOfChats/GameOfChats/NewMessageTableViewController.swift
+++ GameOfChats/GameOfChats/NewMessageTableViewController.swift
@@ -57,25 +57,76 @@ class NewMessageTableViewController: UITableViewController {
         
 //        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
         let user = users[indexPath.row]
         cell.textLabel?.text = user.name
         cell.detailTextLabel?.text = user.email
         
+        
+        if let profileImageURL = user.profileImageUrl {
+            
+            let url = URL(string: profileImageURL)
+            let task = URLSession.shared.dataTask(with: url!) {
+                (data, response, error) in
+                
+                if error != nil {
+                    print(error?.localizedDescription ?? "Profile Image Download Error")
+                    return
+                }
+                
+                // successful
+                DispatchQueue.main.async {
+                    cell.profileImageView.image = UIImage(data: data!)
+                }
+            }
+            task.resume()
+        }
+        
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
 }
 
 
 class UserCell: UITableViewCell {
     
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
         
+        setupViews()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame =  CGRect(x: 56, y: (textLabel?.frame.origin.y)! - 2, width: (textLabel?.frame.width)!, height: (textLabel?.frame.height)!)
+        detailTextLabel?.frame = CGRect(x: 56, y: (detailTextLabel?.frame.origin.y)! + 2, width: (detailTextLabel?.frame.width)!, height: (detailTextLabel?.frame.height)!)
+    }
+    
+    func setupViews() {
+        addSubview(profileImageView)
+        
+        // x, y, width, height
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 }
 
